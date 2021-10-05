8e63eea4e3da40ce6f7fd6d9db3c2b9b225cd8ae
rospiel <rospielberg@gmail.com>
2021-10-05 11:58:42 -0300

    configuração inicial do eureka-server

--- src/main/java/br/com/application/Application.java
+++ src/main/java/br/com/application/Application.java
@@ -2,9 +2,11 @@ package br.com.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import java.util.TimeZone;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
